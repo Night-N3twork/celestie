26ccf3e0b2d40c415c8b7fdeb535a3a59c375595
Sylvia <sylvia@sylvieon.dev>
2026-09-24 15:20:35 +0000

    update dev machine name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		allowedHosts: ["dev.syl.ad"],
+		allowedHosts: ["sylvie", "sylvie.t.syl.ad", "sylvie.adelie-woodpecker.ts.net"],
 	},
 });
